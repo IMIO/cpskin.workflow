f769862ff369ad8bf718ef7c8d8587d71e86c189
Laurent Lasudry <lasudry@gmail.com>
2014-05-08 16:49:05 +0200

    Improve tests

--- cpskin/workflow/testing.py
+++ cpskin/workflow/testing.py
@@ -1,24 +1,14 @@
-from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
-from plone.app.testing import PLONE_FIXTURE
+from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import IntegrationTesting
 
-from zope.configuration import xmlconfig
+import cpskin.workflow
 
 
-class CPSkinWorkflow(PloneSandboxLayer):
-
-    defaultBases = (PLONE_FIXTURE,)
-
-    def setUpZope(self, app, configurationContext):
-        import cpskin.workflow
-        xmlconfig.file('configure.zcml', cpskin.workflow, context=configurationContext)
-
-    def setUpPloneSite(self, portal):
-        applyProfile(portal, 'cpskin.workflow:default')
-
-
-CPSKIN_WORKFLOW_FIXTURE = CPSkinWorkflow()
+CPSKIN_WORKFLOW_FIXTURE = PloneWithPackageLayer(
+    name="CPSKIN_WORKFLOW_FIXTURE",
+    zcml_filename="testing.zcml",
+    zcml_package=cpskin.workflow,
+    gs_profile_id="cpskin.workflow:testing")
 
 CPSKIN_WORKFLOW_INTEGRATION_TESTING = IntegrationTesting(
     bases=(CPSKIN_WORKFLOW_FIXTURE,),
