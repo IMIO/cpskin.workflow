905f80e071647addeb2882870e8d6d72042124c7
Laurent Lasudry <lasudry@gmail.com>
2014-05-20 18:02:40 +0200

    Code refactoring

--- cpskin/workflow/setuphandlers.py
+++ cpskin/workflow/setuphandlers.py
@@ -17,29 +17,17 @@ def installWorkflows(context):
     # we change the default workflow
     logger.info("Adapting default workflow and existing objects")
     wft = getToolByName(portal, 'portal_workflow')
-    tt = getToolByName(portal, 'portal_types')
-    # list types with a non default workflow
-    nondefault = [info[0] for info in wft.listChainOverrides()]
-    # list types with the default workflow
-    type_ids = [type for type in tt.listContentTypes() if type not in nondefault]
-    chain = '(Default)'
     if wft.getDefaultChain() == ('simple_publication_workflow',):
-        wft.setChainForPortalTypes(type_ids, wft.getDefaultChain())
-        wft.setDefaultChain('cpskin_workflow')
         state_map = {'private': 'created',
                      'pending': 'published_and_hidden',
                      'published': 'published_and_hidden'}
-        remap_workflow(portal, type_ids=type_ids, chain=chain, state_map=state_map)
-        wft.setChainForPortalTypes(type_ids, '(Default)')
+        changeDefaultWorkflowAndRemap(portal, state_map, 'cpskin_workflow')
     elif wft.getDefaultChain() == ('plone_workflow',):
-        wft.setChainForPortalTypes(type_ids, wft.getDefaultChain())
-        wft.setDefaultChain('cpskin_workflow')
         state_map = {'private': 'created',
                      'pending': 'published_and_hidden',
                      'published': 'published_and_hidden',
                      'visible': 'published_and_hidden'}
-        remap_workflow(portal, type_ids=type_ids, chain=chain, state_map=state_map)
-        wft.setChainForPortalTypes(type_ids, '(Default)')
+        changeDefaultWorkflowAndRemap(portal, state_map, 'cpskin_workflow')
 
 
 def configureMembers(context):
@@ -72,18 +60,22 @@ def uninstallWorkflows(context):
     # we change the default workflow
     logger.info("Adapting default workflow and existing objects")
     wft = getToolByName(portal, 'portal_workflow')
-    tt = getToolByName(portal, 'portal_types')
-    # list types with a non default workflow
-    nondefault = [info[0] for info in wft.listChainOverrides()]
-    # list types with the default workflow
-    type_ids = [type for type in tt.listContentTypes() if type not in nondefault]
-    wft = getToolByName(portal, 'portal_workflow')
     if wft.getDefaultChain() and wft.getDefaultChain()[0].startswith('cpskin'):
         state_map = {'created': 'private',
                      'pending': 'pending',
                      'published_and_hidden': 'published',
                      'published_and_shown': 'published'}
-        wft.setChainForPortalTypes(type_ids, wft.getDefaultChain())
-        wft.setDefaultChain('simple_publication_workflow')
-        remap_workflow(portal, type_ids=type_ids, chain=('simple_publication_workflow',), state_map=state_map)
-        wft.setChainForPortalTypes(type_ids, '(Default)')
+        changeDefaultWorkflowAndRemap(portal, state_map, 'simple_publication_workflow')
+
+
+def changeDefaultWorkflowAndRemap(portal, state_map, new_wf):
+    wft = getToolByName(portal, 'portal_workflow')
+    tt = getToolByName(portal, 'portal_types')
+    # list types with a non default workflow
+    nondefault = [info[0] for info in wft.listChainOverrides()]
+    # list types with the default workflow
+    type_ids = [type for type in tt.listContentTypes() if type not in nondefault]
+    wft.setChainForPortalTypes(type_ids, wft.getDefaultChain())
+    wft.setDefaultChain(new_wf)
+    remap_workflow(portal, type_ids=type_ids, chain=(new_wf,), state_map=state_map)
+    wft.setChainForPortalTypes(type_ids, '(Default)')
